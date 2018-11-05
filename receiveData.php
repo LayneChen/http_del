11b495a05336521d063af8077fd51004b553ab0c
chenlei <chenlei@haitun.hk>
2018-11-05 15:48:30 +0800

    xiugia

--- receiveData.php
+++ receiveData.php
@@ -1,5 +1,9 @@
 <?php
 
+$body = file_get_contents("php://input");
 if(isset($_REQUEST)){
     file_put_contents('receiveData.txt',date("Y-m-d H:i:s")."[request]:".print_r($_REQUEST,TRUE),FILE_APPEND);
+}
+if(isset($body)){
+    file_put_contents('receiveData.txt',date("Y-m-d H:i:s")."[body]:".print_r($body,TRUE),FILE_APPEND);
 }
