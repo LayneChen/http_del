22217fe5557aa3a2f6ce1faac575dd1228e6d739
chenlei <chenlei@haitun.hk>
2019-12-16 19:50:52 +0800

    aa

--- receiveData.php
+++ receiveData.php
@@ -1,8 +1,20 @@
 <?php
 
+function get_client_ip(){
+    $IP = '';
+    if (getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), 'unknown')) {
+        $IP = getenv('HTTP_CLIENT_IP');
+    } elseif(getenv('HTTP_X_FORWARDED_FOR') && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), 'unknown')) {
+        $IP = getenv('HTTP_X_FORWARDED_FOR');
+    } elseif(getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), 'unknown')) {
+        $IP = getenv('REMOTE_ADDR');
+    } elseif(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], 'unknown')) {
+        $IP = $_SERVER['REMOTE_ADDR'];
+    }
+    return $IP ? $IP : "unknow";
+}
 
-
-file_put_contents('receiveData.txt',date("Y-m-d H:i:s")."[header]:".print_r(getallheaders(),TRUE),FILE_APPEND);
+file_put_contents('receiveData.txt',date("Y-m-d H:i:s")."[header]:".print_r(get_client_ip(),TRUE),FILE_APPEND);
 
 $body = file_get_contents("php://input");
 if(isset($_REQUEST)){
